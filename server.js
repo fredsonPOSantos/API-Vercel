0572f0d25f3b034cdb4586520a12b96586d4ed65
fredsonPOSantos <son.magometal@gmail.com>
2024-11-03 23:15:43 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,9 +11,8 @@ const app = express(); // Inicializa o Express
 const secret = process.env.JWT_SECRET; // Token secreto para autenticação
 const tokenExpiration = '1h'; // Duração do token JWT
 
-const https = require('https');
-
-const httpsPort = process.env.HTTPS_PORT || 3002; // Porta para o servidor HTTPS;
+const http = require('http');
+const httpPort = process.env.HTTP_PORT || 4000; // Porta para o servidor HTTP
 
 dotenv.config(); // Serve Para Carregar Variaveis de Ambiente
 require('dotenv').config();  //carregar variaveis de ambiente
@@ -63,7 +62,7 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
     .then(() => console.log('Banco de Dados MongoDB-Cloud connectado com Sucesso'))
     .catch(err => console.log(err));
 
-
-https.createServer(options, app).listen(httpsPort, () => {
-  console.log(`Servidor HTTPS rodando em https://localhost:${httpsPort}`);
+// Iniciar o servidor HTTP
+http.createServer(app).listen(httpPort, () => {
+  console.log(`Servidor HTTP rodando em http://localhost:${httpPort}`);
 });
